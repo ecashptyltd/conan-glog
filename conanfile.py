fbcd154ccd476369cc127e97a5ec979a2ecb3408
Uilian Ries <uilianries@gmail.com>
2018-10-09 13:29:08 -0300

    Update recipe to new template
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,8 +9,10 @@ class GlogConan(ConanFile):
     name = "glog"
     version = "0.3.5"
     url = "https://github.com/bincrafters/conan-glog"
+    homepage = "https://github.com/gflags/gflags"
     description = "Google logging library"
     license = "BSD 3-Clause"
+    author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt"]
     source_subfolder = "source_subfolder"
@@ -26,26 +28,28 @@ class GlogConan(ConanFile):
         if self.options.with_gflags:
             self.requires("gflags/2.2.1@bincrafters/stable")
         
-
     def source(self):
         source_url =  "https://github.com/google/{0}".format(self.name)
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
-    def build(self):
+    def configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions['WITH_GFLAGS'] = self.options.with_gflags
         cmake.definitions['WITH_THREADS'] = self.options.with_threads
         cmake.definitions['BUILD_TESTING'] = False
-        if self.settings.os != "Windows":
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
         cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
         self.copy("COPYING", dst="licenses", src=self.source_subfolder)
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
